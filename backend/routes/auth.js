a8848f4561cfaa1279df6151b585941dc1b2afbf
Ankit Sharma <sharmaankit.as1995@gmail.com>
2022-07-08 21:45:44 +0530

    initial commit

---
+++ backend/routes/auth.js
@@ -0,0 +1,17 @@
+const { application } = require('express');
+const express =require ('express');
+const User =require('../models/User');
+const router = express.Router();
+
+
+
+router.get('/', (req, res) =>{
+    console.log(req.body)
+    const user = User(req.body);
+    user.save()
+    res.json(req.body)
+
+}
+
+)
+module.exports = router
